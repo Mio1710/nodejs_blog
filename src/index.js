102a19e3ecacb430f67259b6063ec709a612a1a1
NGUYENTHILE\leeee <le.nt@qasoftsolution.com>
2024-03-17 16:02:01 +0700

    feat: route and controller

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const path = require("path");
+const route = require('./routes');
 const app = express()
 const port = 3000
 
@@ -14,13 +15,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+//Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
